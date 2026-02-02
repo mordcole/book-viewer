0c6ee2a48701b90aebdd784dfca8133dbb4ed5be
mordcoleman <105605981+Mcolemangit@users.noreply.github.com>
2026-02-02 16:13:02 -0500

    changed address

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,6 @@ const port = 3000;
 app.use("/api", apiRoutes);
 app.use(docsRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
